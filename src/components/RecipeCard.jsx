19a33156dc51767dcbde33a2ca1aa391760edb33
Saif Maknojia <saif.maknojia@outlook.com>
2023-03-09 00:31:47 -0500

    fixed card on mobile and tablet screen

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import {Card, CardHeader, CardMedia, Grid} from '@mui/material';
-import {useNavigate} from 'react-router-dom';
+import React from "react";
+import { Card, CardHeader, CardMedia, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const RecipeCard = props => {
-    const {title, image, id} = props;
-    const navigate = useNavigate();
-    return (
-        <Grid item xs={4}>
-            <Card 
-                sx={{ maxWidth: 345 }} 
-                style={{cursor: 'pointer'}}
-                onClick={() => navigate(`/recipe/${id}`)}
-            >
-                <CardHeader title={title}/>
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image={image}
-                    alt={title}
-                />
-            </Card>
-        </Grid>
-    );
-}
+const RecipeCard = (props) => {
+  const { title, image, id } = props;
+  const navigate = useNavigate();
+  return (
+    <Grid item xs={12} md={6} lg={4}>
+      <Card
+        sx={{ maxWidth: 345 }}
+        style={{ cursor: "pointer" }}
+        onClick={() => navigate(`/recipe/${id}`)}
+      >
+        <CardHeader title={title} />
+        <CardMedia component="img" height="194" image={image} alt={title} />
+      </Card>
+    </Grid>
+  );
+};
 export default RecipeCard;
